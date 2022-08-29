d60846e6c2ff4c94a492eb4e7df90a7ffb3dd28b
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-08-29 23:07:59 +0300

    simple css

---
+++ src/components/App/App.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import { Container, SectionTitle, PageTitle } from "./App.styled";
+
+export class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    
+  }
+
+  render() {
+    return (
+      <Container>
+        <PageTitle>GoIT React HW 3 Image finder</PageTitle>
+        <SectionTitle>other title</SectionTitle>
+      </Container>
+    );
+  }
+};
